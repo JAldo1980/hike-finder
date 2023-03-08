b3632eb45197596a89a0d6cca235517e5694ccd5
James Alderman <80925002+JAldo1980@users.noreply.github.com>
2023-03-08 13:10:20 +0000

    code updates

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-import { destinations } from "./destinations.js";
+const hamburgerIcon = document.querySelector(".hamburger-icon");
+const mobileNav = document.querySelector(".mobile-nav");
+
+hamburgerIcon.addEventListener("click", function () {
+  hamburgerIcon.classList.toggle("active");
+  hamburgerIcon.classList.toggle("transform-white");
+  mobileNav.classList.toggle("show-nav");
+});
