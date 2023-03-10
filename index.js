93f6d03aa2a1449bdf04ec2a3135916a2423c52d
James Alderman <80925002+JAldo1980@users.noreply.github.com>
2023-03-10 15:58:49 +0000

    js updates

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const hamburgerIcon = document.querySelector(".hamburger-icon");
 const mobileNav = document.querySelector(".mobile-nav");
+const signupBtn = document.querySelector(".signup-btn");
+const loginBtn = document.querySelector(".login-btn");
 
 hamburgerIcon.addEventListener("click", function () {
   hamburgerIcon.classList.toggle("active");
   hamburgerIcon.classList.toggle("transform-white");
   mobileNav.classList.toggle("show-nav");
+});
+
+signupBtn.addEventListener("click", function () {
+  console.log("signup click!");
+});
+
+loginBtn.addEventListener("click", function () {
+  console.log("login click!");
 });
